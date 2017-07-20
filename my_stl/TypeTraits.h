18d1f45b6cc6a9b2a45690e5a7b9edf5674eed59
gaoxiaodiao <805888196@qq.com>
2017-07-20 19:14:25 +0800

    类型萃取

---
+++ my_stl/TypeTraits.h
@@ -0,0 +1,39 @@
+/*
+*文件说明:类型萃取
+*作者:高小调
+*日期:2017-07-20
+*集成开发环境:Microsoft Visual Studio 2010 
+*/
+#ifndef __TYPETRAITS_H__
+#define __TYPETRAITS_H__
+struct _TrueType {};
+struct _FalseType {};
+
+template<typename T>
+struct _TypeTraits{
+	//暂时精简为一个成员
+	typedef _FalseType    Is_POD_Type;
+};
+
+template<>
+struct _TypeTraits<int>{
+	typedef _TrueType    Is_POD_Type;
+};
+
+template<>
+struct _TypeTraits<unsigned int>{
+	typedef _TrueType    Is_POD_Type;
+};
+
+template<>
+struct _TypeTraits<char>{
+	typedef _TrueType    Is_POD_Type;
+};
+
+template<>
+struct _TypeTraits<unsigned char>{
+	typedef _TrueType    Is_POD_Type;
+};
+//......
+
+#endif
