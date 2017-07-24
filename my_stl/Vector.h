a81369a9ac605e488519368cc244045d84bf10cf
gaoxiaodiao <805888196@qq.com>
2017-07-24 15:52:31 +0800

    添加注释,修改一些代码

--- my_stl/Vector.h
+++ my_stl/Vector.h
@@ -16,13 +16,16 @@ public:
 	typedef T* Iterator;
 	typedef const T* ConstIterator;
 public:
+	//缺省构造函数
 	Vector():_start(0),_finish(0),_endOfStorage(0){}
+	//拷贝构造函数
 	Vector(const Vector& v):_start(0),_finish(0),endOfStorage(0){
 		size_t n = v.End()-v.Begin();
 		_start = AllocateAndCopy(n,v.Begin(),v.End());
 		_finish = _start + n;
 		_endOfStorage = _finish;
 	}
+	//赋值运算符重载
 	Vector & operator=(const Vector& v){
 		if(v._start == _start){
 			Vector<T> tmp(v);
@@ -30,17 +33,18 @@ public:
 		}
 		return *this;
 	}
+	//析构函数
 	~Vector(){Deallocate();}
-
 public:
+	//尾插
 	void PushBack(const T &val){
-		_CheckCapacity();//这句必须加上,防止第一次进来迭代器为0
 		Insert(End(),val);
 	}
+	//头插
 	void PushFront(const T & val){
-		_CheckCapacity();
 		Insert(Begin(),val);
 	}
+	//插入
 	void Insert(Iterator pos,const T & val){
 		_CheckCapacity();
 		Iterator end = End();
@@ -52,18 +56,21 @@ public:
 		Construct(&*pos,val);//*pos = val; 错误!
 		_finish++;
 	}
+	//头删
 	void PopFront(){
 		if(Empty()){
 			return ;
 		}
 		Erase(Begin()):
 	}
+	//尾删
 	void PopBack(){
 		if(Empty()){
 			return ;
 		}
 		Erase(End()-1);
 	}
+	//删除
 	Iterator Erase(Iterator pos){
 		Iterator end = End();
 		while(pos < end-1){
@@ -94,7 +101,7 @@ protected:
 			int oldCapacity = _endOfStorage - _start;
 			int newCapacity = oldCapacity*2+3;
 			int size = End() - Begin();
-			//将原来的数据拷贝到新容量中
+			//开辟新空间并将原来的数据拷贝到新容量中
 			Iterator newBase = AllocateAndCopy(newCapacity,Begin(),End());
 			//析构现有数据
 			Deallocate();
@@ -105,21 +112,28 @@ protected:
 		}
 	
 	}
+	//开辟一段空间并拷贝
 	Iterator AllocateAndCopy(size_t n,Iterator first,Iterator end){
 		Iterator ret = Allocator::Allocate(n);
 		UninitializedCopy(first,end,ret);
 		return ret;
 	}
+	//调用析构函数并释放空间
 	void Deallocate(){
 		Destroy(_start,_finish);						//调用析构函数
 		Allocator::Deallocate(_start,_endOfStorage-_start);	//释放空间
-		_start = _finish = _endOfStorage = 0;				//
+		_start = _finish = _endOfStorage = 0;				
 	}
 public:
+	//迭代器相关函数
 	Iterator Begin(){
+		//在获取Begin时,先进行扩容判断,以免迭代器失效
+		_CheckCapacity();
 		return _start;
 	}
 	Iterator End(){
+		//同上
+		_CheckCapacity();
 		return _finish;
 	}
 private:
